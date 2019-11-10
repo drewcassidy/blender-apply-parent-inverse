fa9dd340b3e5b9131f66fdbf9e57154918e99bc4
drewcassidy <drewcassidy@me.com>
2019-11-09 23:51:01 -0800

    Add keymap handling and menu items

--- apply_parent_inverse.py
+++ apply_parent_inverse.py
@@ -45,12 +45,35 @@ class ApplyParentInverse(bpy.types.Operator):
         return {'FINISHED'}  # Lets Blender know the operator finished successfully.
 
 
+def menu_func(self, context):
+    self.layout.operator(ApplyParentInverse.bl_idname)
+
+
 def register():
     bpy.utils.register_class(ApplyParentInverse)
+    bpy.types.VIEW3D_MT_object.append(menu_func)
+
+    # handle the keymap
+    wm = bpy.context.window_manager
+    # Note that in background mode (no GUI available), keyconfigs are not available either,
+    # so we have to check this to avoid nasty errors in background case.
+    kc = wm.keyconfigs.addon
+    if kc:
+        km = wm.keyconfigs.addon.keymaps.new(name='Object Mode', space_type='EMPTY')
+        kmi = km.keymap_items.new(ApplyParentInverse.bl_idname, 'P', 'PRESS', ctrl=False, shift=True, alt=True)
+        addon_keymaps.append((km, kmi))
 
 
 def unregister():
+    # Note: when unregistering, it's usually good practice to do it in reverse order you registered.
+    # Can avoid strange issues like keymap still referring to operators already unregistered...
+    # handle the keymap
+    for km, kmi in addon_keymaps:
+        km.keymap_items.remove(kmi)
+    addon_keymaps.clear()
+
     bpy.utils.unregister_class(ApplyParentInverse)
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
 
 
 # This allows you to run the script directly from Blender's Text editor
